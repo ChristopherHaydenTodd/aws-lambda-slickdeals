02459e81d17482ad3c4a7a4b1bdaa6325eedcb67
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2020-04-29 13:34:22 -0400

    updating formatting of logs, adding args, adding docstring

--- environment/install_libraries.sh
+++ environment/install_libraries.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
+# -----------------------------------------------------------------------------
+# Install Required Libraries in the correct location (ensures)
+# Virtual Environment is correct. This is required as the libraries
+# need to be properly packaged with the code for deployment.
 #
-# Install Required Libraries
+# Arguments:
+# N/A
 #
+# Example:
+# sh install_libraries.sh
+# -----------------------------------------------------------------------------
 
-if [[ -z "$VIRTUAL_ENV" ]]
-then
-  echo "Python Virtual Environment is NOT Active. You need to be active to install libaries"
+
+###
+# Helper Functions and Setup
+###
+
+
+set -e
+
+function log {
+  echo "$(date +%c) $1: $2"
+}
+
+
+###
+# Check Environment
+###
+
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+  log "ERROR" "Python Virtual Environment is NOT Active. You need to be active to install libaries"
+  exit 1
+elif [[ "$VIRTUAL_ENV" != *"slickdeals-venv"* ]]; then
+  log "ERROR" "Incorrect Python Virtual Environment is set, need 'slickdeals-venv' not '$VIRTUAL_ENV'"
   exit 1
 fi
 
-echo "$(date +%c): Installing Required Packages"
+
+###
+# Execute Commands
+###
+
+
+log "INFO" "Installing Required Packages"
 pip3 install -r requirements.txt
