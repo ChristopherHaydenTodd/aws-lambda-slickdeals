ebfedbbc24c7e7f7009f0ed06036ed5ca8620f3f
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2020-04-30 16:46:11 -0400

    adding script for running tests against the package

---
+++ test_python_package.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# -----------------------------------------------------------------------------
+# Test Python Package using setuptools, pytest, pytest-cov
+#
+# Arguments:
+# -c, --cov
+#    Open/Run code coverage report
+#
+# Example:
+# sh test_python_package.sh [-h] [-c]
+# -----------------------------------------------------------------------------
+
+
+###
+# Helper Functions and Setup
+###
+
+
+function log {
+  echo "$(date +%c) $1: $2"
+}
+
+
+###
+# Arguments
+###
+
+
+SETUP_FILE="./setup.py"
+OPEN_CODE_COVERAGE=false
+
+# Parse CLI Arguments
+while [ "$#" -gt 0 ]
+do
+    key="$1"
+
+    case $key in
+        -c|--cov)
+        OPEN_CODE_COVERAGE=true
+        shift
+        ;;
+        -h|--help)
+        echo "Usage: sh test_python_package.sh [-h] [-c]"
+        exit 1
+        ;;
+        *)
+        shift
+        ;;
+        *)
+        log "ERROR" "Unknown argument \"$1\", exiting"
+        exit 1
+        ;;
+    esac
+done
+
+
+###
+# Execute Commands
+###
+
